c941a6cbe8bc82647d31951df8b8248f8356c0b3
Ian <glps1004@gmail.com>
2023-01-18 23:10:52 +0800

    scripts

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+/*document.getElementById('submit').onclick = function() {
+    var radio = document.querySelector('input[type=radio][name=language]:checked');
+    radio.checked = false;
+}*/
+alert("測試中...滾")
