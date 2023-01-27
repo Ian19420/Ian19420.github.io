815d2bd0e4eb9e060f15b1379499c8d4e0564920
Ian <glps1004@gmail.com>
2023-01-28 01:37:26 +0800

    revise

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ setInterval(a, 10);
 
 var Btnsubmit = document.querySelector("[data-action = 'submit']");
 Btnsubmit.onclick = function (){
-    var sheet_url = "https://script.google.com/macros/s/AKfycbxNhikis3sVIDPpniXUaWRycPCMT1Apcnd6rVMj6zLh4caMvmfUtYMj0EyvNCZ7rGQc/exec";
+    var sheet_url = "https://script.google.com/macros/s/AKfycbyHRGezoGVs58M4_lXWKMJTzvhiBRdqfCpn6WdK5lWC14je2P4MuGqhqNVQ0iXx344p/exec";
     var lunch = Btn.value.split(" ", 2)[0];
     var price = Btn.value.split(" ", 2)[1];
     var myselect = document.querySelector("[name = 'number']");
@@ -38,14 +38,17 @@ Btnsubmit.onclick = function (){
         },
         dataType: "JSON",
         success: function(response) {
-            console.log(response);
+        console.log(response);
         if(response == "success")
         {
             alert("success");
             clear();
         }
         },
-        //error: function(response)
+        error: function(response){
+            console.log(response);
+            alert("error");
+        }
     });
 }
 function clear()
